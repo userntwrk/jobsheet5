a54d68ad77bed888c8b11dd4f89d9702bd3f4d0f
wahyu <userblank21@gmail.com>
2022-03-31 10:19:33 +0700

    jobsheet5

---
+++ lib/pages/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:jobsheet5/pages/home_page.dart';
+import 'package:jobsheet5/pages/item_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => ItemPage(),
+    },
+  ));
+}
